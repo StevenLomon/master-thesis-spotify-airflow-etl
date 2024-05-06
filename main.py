6acc5ef14be745c8497d80f96d98402dbe3a77fd
StevenLomon <steven.lennartsson@gmail.com>
2024-05-06 08:27:49 +0200

    We are getting data! We have the 5 most popular playlists in Sweden

--- main.py
+++ main.py
@@ -1,7 +1,50 @@
-import requests, rich
+import requests
 import pandas as pd
 from dotenv import dotenv_values
+from rich import print, print_json
 
 config = dotenv_values(".env")
-client_ID = config['CLIENT_ID']
-print(client_ID)
+CLIENT_ID = config['CLIENT_ID']
+CLIENT_SECRET = config['CLIENT_SECRET']
+
+AUTH_URL = 'https://accounts.spotify.com/api/token'
+redirect_uri = 'http://localhost:8888/callback'
+
+# POST
+auth_response = requests.post(AUTH_URL, {
+    'grant_type': 'client_credentials',
+    'client_id': CLIENT_ID,
+    'client_secret': CLIENT_SECRET,
+})
+
+# convert the response to JSON
+auth_response_data = auth_response.json()
+
+# save the access token
+access_token = auth_response_data['access_token']
+
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
+
+# base URL of all Spotify API endpoints
+BASE_URL = 'https://api.spotify.com/v1/'
+
+artist_id = '36QJpDe2go2KgaRleHCDTp'
+
+# get the 5 most popular playlists in Sweden right now
+r = requests.get(BASE_URL + 'browse/featured-playlists', 
+                 headers=headers, 
+                 params={'locale': 'sv_SE', 'limit': 5})
+d = r.json()
+
+print(d)
+
+refined_playlists = []
+for playlist in d['playlists']['items']:
+    refined_playlists.append({'id': playlist['id'], 'name': playlist['name'], 
+                              'description': playlist['description'], 'tracks': playlist['tracks']['total']})
+
+print(refined_playlists)
+# for playlist in d['playlists']:
+#     print(f"{playlist['href']}")
